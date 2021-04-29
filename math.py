c3800788d41c04820edf0cc5fe7e7105f585a7b2
SrushtiKharabe <41324563+SrushtiKharabe@users.noreply.github.com>
2021-04-29 09:18:57 +0530

    Initial commit math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add Implementataion
+def add(x,y):
+    pass
+    return (x+Y)
+#Subtract Implimentation
+def subtract(x,y):
+    return x-y
+#Multiply Implimentation
+def multiply(x,y):
+    pass
+    return x*y
+#Duvide Implimentation
+def divide(x,y):
+
+    if y==0
+        return DIVIDE_BY_0_ERROR  #bug789 impl
+    else    
+        return x/y
+
+def square(x):
+    return x*x
